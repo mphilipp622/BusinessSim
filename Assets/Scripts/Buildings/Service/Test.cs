5910af08ea950ef6433e47fe0a9bcd4c7698212b
Mark Philipp <mphilipp622@gmail.com>
2019-09-05 14:41:49 -0700

    Updated for Unity 2019

---
+++ Assets/Scripts/Buildings/Service/Test.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Test : ServiceBuilding 
+{
+ 
+	void Start () 
+	{
+ 
+	}
+ 
+	void Update () 
+	{
+ 
+	}
+}
